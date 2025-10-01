311bc0c22806b3e12358464c853e52c8b95e2f5f
Ahmad <theblindmatt@gmail.com>
2025-09-30 20:08:08 -0600

    tuff code (added .d)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
@@ -68,7 +68,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Disabled
 public class BasicOmniOpMode_Linear extends LinearOpMode {
 
-    // Declare OpMode members for each of the 4 motors.
+    // Declare OpMode members for each of the 4 motors. d
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftDrive = null;
     private DcMotor backLeftDrive = null;
